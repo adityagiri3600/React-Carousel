e140874025c0835b45eb1b4b5a740f2b50d7fc16
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2024-03-26 08:48:15 +0530

    added loopback functionality

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1> {index} </h1>
-      <Carousel transitionTime={0.3} setIndex={setIndex}>
+      <Carousel transitionTime={0.3} getIndex={setIndex} loopback={true}>
         <div style={{ backgroundColor: 'red', height: '100px', width: '200px' }}>1</div>
         <div style={{ backgroundColor: 'green', height: '100px', width: '200px' }}>2</div>
         <div style={{ backgroundColor: 'blue', height: '100px', width: '200px' }}>3</div>
